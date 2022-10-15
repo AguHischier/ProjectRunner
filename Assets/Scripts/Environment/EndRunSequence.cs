036be03286422a927ebbb44a42c038904f7d39bc
Agustin Hischier <63270815+AguHischier@users.noreply.github.com>
2022-10-14 23:20:58 -0300

    End

---
+++ Assets/Scripts/Environment/EndRunSequence.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndRunSequence : MonoBehaviour
+{
+
+    public GameObject liveCoins;
+    public GameObject liveDis;
+    public GameObject endScreen;
+    public GameObject fadeOut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EndSequence());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator EndSequence()
+    {
+        yield return new WaitForSeconds(5);
+        liveDis.SetActive(false);
+        liveCoins.SetActive(false);
+        endScreen.SetActive(true);
+        yield return new WaitForSeconds(5);
+        fadeOut.SetActive(true);
+    }
+}
--- Assets/Scripts/Environment/Obstaclecollision.cs
+++ Assets/Scripts/Environment/Obstaclecollision.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Obstaclecollision : MonoBehaviour
 {
     public GameObject thePlayer;
-   // public GameObject charModel; ESTO ES PARA PONER EL ANIMADOR DEL PJ
+    // public GameObject charModel; ESTO ES PARA PONER EL ANIMADOR DEL PJ
+    public GameObject mainCam;
+    public GameObject levelControl;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         //charModel.GetComponent<Animator>().Play(""); ACA VA LA ANIMACION DE MORIR
+        levelControl.GetComponent<LevelDistance>().enabled = false;
+        mainCam.GetComponent<Animator>().enabled = true;
+        levelControl.GetComponent<EndRunSequence>().enabled = true;
     }
 }
